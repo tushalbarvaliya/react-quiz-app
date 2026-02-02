2d96fbdf580f2374122a5b4b77a503584ad682f5
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-02 14:24:02 +0530

    feat:header section create

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { Header } from "./components/Header";
+
 function App() {}
 
-export default App;
+export default App = () => {
+    return (
+        <>
+        <Header></Header>
+        </>
+    );
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../assets/quiz-logo.png'
+export const Header = () => {
+  return (
+    <header>
+        <img src={logo} alt="" />
+        <h2>Quiz App(React)</h2>
+    </header>
+  )
+}
